5788d82b63f91f952f0bb8d2ed55420834b39fdd
Uvin Weththasinghe <uvin6667@gmail.com>
2020-11-23 19:37:07 +0530

    Add changes for the new game

--- js/script.js
+++ js/script.js
@@ -1,26 +1,32 @@
-var done = false;
-var count =1;
-var buttonElement = $('button');
-buttonElement.click(function (){
-    done=false;
-    count++;
-})
-$('body').on('click',function (event){
-    if($(event.target).hasClass('box')){
-        if(done){
-            $(event.target).css('backgroundColor','green');
-        }
-        else {
-            if(count%2===0){
-                $(event.target).css('backgroundColor','blue');
-                done = true;
-            }
-            else{
-                $(event.target).css('backgroundColor','yellow');
-                done = true;
-            }
+var stageElement = $('#stage');
+var balls = [];
+for(var i=0;i<10;i++){
+    var ballElement = $('<div></div>');
+    ballElement.addClass('balls');
+    stageElement.append(ballElement)
+    balls.push(ballElement);
+    ballElement.css('backgroundColor',randomColor());
 
-        }
+}
+
+setInterval(function (){
+    for(var j=0; j < balls.length; j++){
+        $(balls[j]).css('left',''+($('body').innerWidth()-$(balls[j]).innerWidth())*Math.random()+'px');
+        $(balls[j]).css('top',''+($('body').innerHeight()-$(balls[j]).innerHeight())*Math.random()*0.5+'px');
+/*
+        $(balls[j]).css('borderRadius',Math.random()*100+'%');
+*/
     }
-})
+},1000);
 
+
+function randomColor() {
+    var r = Math.random() * 256;
+    var g = Math.random() * 256;
+    var b = Math.random() * 256;
+    return 'rgb(' + r + ',' + g + ',' + b + ')'
+}
+
+$('body').mousemove(function (event){
+    $('#gun').css('left',event.clientX+'px');
+})
