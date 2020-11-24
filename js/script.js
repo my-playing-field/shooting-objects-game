7fdbefc684df5b024d381cc3b9ab7e09e5a9a40e
Uvin Weththasinghe <uvin6667@gmail.com>
2020-11-24 06:13:27 +0530

    Add the changes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 var stageElement = $('#stage');
 var balls = [];
-for(var i=0;i<10;i++){
+for(var i=0;i<20;i++){
     var ballElement = $('<div></div>');
     ballElement.addClass('balls');
     stageElement.append(ballElement)
     balls.push(ballElement);
+/*
     ballElement.css('backgroundColor',randomColor());
+*/
 
 }
+
+var height = $('body').innerHeight();
 
 setInterval(function (){
     for(var j=0; j < balls.length; j++){
@@ -16,17 +20,42 @@ setInterval(function (){
 /*
         $(balls[j]).css('borderRadius',Math.random()*100+'%');
 */
+
     }
 },1000);
 
 
-function randomColor() {
+/*function randomColor() {
     var r = Math.random() * 256;
     var g = Math.random() * 256;
     var b = Math.random() * 256;
     return 'rgb(' + r + ',' + g + ',' + b + ')'
-}
+}*/
 
 $('body').mousemove(function (event){
     $('#gun').css('left',event.clientX+'px');
+    $('#bullet').css('left',event.clientX+'px');
 })
+
+$('body').click(function (){
+    $('#bullet').css('bottom',height+'px');
+})
+
+/*setInterval(function (){
+    for(var k=0; k<balls.length;k++){
+        if($(balls[k]).offset().top - $('#gun').offset().top > 100  &&
+            $(balls[k]).offset().left - $('#gun').offset().left >100){
+            $(balls[k]).css('backgroundColor','red');
+        }
+    }
+},1);*/
+
+setInterval(function (){
+    for(var l=0;l<balls.length;l++){
+        if(Math.abs($(balls[l]).offset().left-$('#gun').offset().left)<50){
+            $(balls[l]).css('backgroundColor','transparent');
+        }
+    }
+},1000);
+
+
